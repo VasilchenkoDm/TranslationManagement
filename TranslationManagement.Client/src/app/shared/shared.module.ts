f34266621b6dde09b544fcd46512bb47ab781b84
Dmitriy Vasilchenko <vasilchenko.dm@gmail.com>
2024-03-13 09:53:38 +0100

    Add table on TranslationJobListComponent

--- TranslationManagement.Client/src/app/shared/shared.module.ts
+++ TranslationManagement.Client/src/app/shared/shared.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from "@angular/core";
+import { RouterModule } from "@angular/router";
 import { LayoutComponent } from "./layout/layout.component";
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
@@ -7,7 +8,8 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatListModule } from '@angular/material/list';
 import { MatExpansionModule } from '@angular/material/expansion';
-import { RouterModule } from "@angular/router";
+import { MatTableModule } from "@angular/material/table";
+import { MatSortModule } from "@angular/material/sort";
 
 @NgModule({
     imports: [
@@ -18,10 +20,23 @@ import { RouterModule } from "@angular/router";
         MatMenuModule,
         MatListModule,
         MatExpansionModule,
-        MatButtonModule
+        MatButtonModule,
+        MatTableModule,
+        MatSortModule
     ],
     declarations: [
         LayoutComponent
+    ],
+    exports: [
+        MatSidenavModule,
+        MatToolbarModule,
+        MatIconModule,
+        MatMenuModule,
+        MatListModule,
+        MatExpansionModule,
+        MatButtonModule,
+        MatTableModule,
+        MatSortModule
     ]
 })
 export class SharedModule { 
--- TranslationManagement.Client/src/app/translation-job/list/translation-job-list.component.ts
+++ TranslationManagement.Client/src/app/translation-job/list/translation-job-list.component.ts
@@ -1,4 +1,6 @@
-import { Component } from "@angular/core";
+import { Component, ViewChild } from "@angular/core";
+import { MatSort } from "@angular/material/sort";
+import { MatTableDataSource } from "@angular/material/table";
 
 @Component({
     selector: 'app-translation-job-list',
@@ -6,4 +8,25 @@ import { Component } from "@angular/core";
     styleUrls: ['./translation-job-list.component.scss']
 })
 export class TranslationJobListComponent{
+    @ViewChild(MatSort, { static: false }) sort!: MatSort;
+    
+    public translationJobs: Array<any> = [{ name: 'test 1'}, { name: 'test 2'}, { name: 'test 3'}];
+    public dataSource: MatTableDataSource<any> = new MatTableDataSource(this.translationJobs);
+    public displayedColumns: string[] = ['Name'];
+
+    constructor(){
+
+    }
+
+    public createJob(): void {       
+    }
+
+    public selectJob(event: MouseEvent, job: any): void {
+
+    }
+
+    public sortJobData(): void {
+        
+    }
+
 }
