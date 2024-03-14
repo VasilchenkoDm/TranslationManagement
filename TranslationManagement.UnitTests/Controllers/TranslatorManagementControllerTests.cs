76ba02baea2966ceb3e5120a2ecf0c00b1d31606
Dmitriy Vasilchenko <vasilchenko.dm@gmail.com>
2024-03-14 12:25:24 +0100

    add UnitTests

---
+++ TranslationManagement.UnitTests/Controllers/TranslatorManagementControllerTests.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TranslationManagement.Api.Controlers;
+using TranslationManagement.BusinessLogic.Services.Interfaces;
+using TranslationManagement.ViewModels.Translator;
+
+namespace TranslationManagement.UnitTests.Controllers
+{
+    public class TranslatorManagementControllerTests
+    {
+        private readonly ITranslatorService _translatorService;
+
+        public TranslatorManagementControllerTests()
+        {
+            _translatorService = A.Fake<ITranslatorService>();
+        }
+
+        [Fact]
+        public async void GetTranslatorsShouldBeOkObjectResult()
+        {
+            //Arrange 
+            var controller = new TranslatorManagementController(_translatorService);
+
+            //Act
+            var result = await controller.GetTranslators();
+
+            //Assert
+            result.Should().BeOfType(typeof(OkObjectResult));
+        }
+
+        [Fact]
+        public async void AddTranslatorShouldBeNoContentResult()
+        {
+            //Arrange 
+            var translatorModel = A.Fake<RequestAddTranslatorModel>();
+            var controller = new TranslatorManagementController(_translatorService);
+
+            //Act
+            var result = await controller.AddTranslator(translatorModel);
+
+            //Assert
+            result.Should().BeOfType(typeof(NoContentResult));
+        }
+
+        [Fact]
+        public async void UpdateTranslatorStatusShouldBeNoContentResult()
+        {
+            //Arrange 
+            var updateStatusTranslatorModel = A.Fake<RequestUpdateStatusTranslatorModel>();
+            var controller = new TranslatorManagementController(_translatorService);
+
+            //Act
+            var result = await controller.UpdateTranslatorStatus(updateStatusTranslatorModel);
+
+            //Assert
+            result.Should().BeOfType(typeof(NoContentResult));
+        }
+
+    }
+}
---
+++ TranslationManagement.UnitTests/Services/TranslatorServiceTests.cs
@@ -0,0 +1,90 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using TranslationManagement.BusinessLogic.Services;
+using TranslationManagement.DataAccess.Repositories.Interfaces;
+using TranslationManagement.Shared.Enums;
+using TranslationManagement.ViewModels.Translator;
+
+namespace TranslationManagement.UnitTests.Services
+{
+    public class TranslatorServiceTests
+    {
+        private readonly ITranslatorRepository _translatorRepository;
+        private readonly ILogger<TranslatorService> _logger;
+        private readonly IMapper _mapper;
+
+        public TranslatorServiceTests()
+        {
+            _translatorRepository = A.Fake<ITranslatorRepository>();
+            _logger = A.Fake<ILogger<TranslatorService>>();
+            _mapper = A.Fake<IMapper>();
+        }
+
+        [Fact]
+        public async void GetListShouldNotBeNull()
+        {
+            //Arrange 
+            var service = new TranslatorService(_mapper, _logger, _translatorRepository);
+
+            //Act
+            var result = await service.GetList();
+
+            //Assert
+            result.Should().NotBeNull();
+        }
+
+        [Fact]
+        public async void AddShouldNotThrow()
+        {
+            //Arrange 
+            var translatorModel = A.Fake<RequestAddTranslatorModel>();
+            var service = new TranslatorService(_mapper, _logger, _translatorRepository);
+
+            //Act                       
+            Func<Task> result = async () => {
+                await service.Add(translatorModel);
+            };
+
+            //Assert
+            await result.Should().NotThrowAsync();
+        }
+
+        [Fact]
+        public async void UpdateStatusShouldNotThrow()
+        {
+            //Arrange 
+            var updateStatusTranslatorModel = A.Fake<RequestUpdateStatusTranslatorModel>();
+            updateStatusTranslatorModel.Status = TranslatorStatusEnum.Certified.ToString();
+
+            var service = new TranslatorService(_mapper, _logger, _translatorRepository);
+
+            //Act                       
+            Func<Task> result = async () => {
+                await service.UpdateStatus(updateStatusTranslatorModel);
+            };
+
+            //Assert
+            await result.Should().NotThrowAsync();
+        }
+
+        [Fact]
+        public async void UpdateStatusShouldThrowArgumentException()
+        {
+            //Arrange 
+            var updateStatusTranslatorModel = A.Fake<RequestUpdateStatusTranslatorModel>();
+            updateStatusTranslatorModel.Status = string.Empty;
+
+            var service = new TranslatorService(_mapper, _logger, _translatorRepository);
+
+            //Act                       
+            Func<Task> result = async () => {
+                await service.UpdateStatus(updateStatusTranslatorModel);
+            };
+
+            //Assert
+            await result.Should().ThrowExactlyAsync<ArgumentException>();
+        }
+
+    }
+}
